68cd118eaaa34e00782c7325a0475078cc1eff4c
amnakamran1 <Amnakamran2024@gmail.com>
2024-11-15 20:26:01 -0800

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,AboutComponent],
+  imports: [RouterOutlet,HeaderComponent,AboutComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
