958c3d7ebdc2bfc5120344a956c2a96834fd556d
amnakamran1 <Amnakamran2024@gmail.com>
2024-11-16 21:18:49 -0800

    Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from './header/header.component';
+// import { AboutComponent } from './about/about.component';
+// import { HomeComponent } from './home/home.component';
+// import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,AboutComponent,HomeComponent],
+  imports: [RouterOutlet,RouterLink,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
