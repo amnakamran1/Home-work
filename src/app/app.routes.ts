3b1bee3ac3cc61f5e7f2f363847e1b1447195543
amnakamran1 <Amnakamran2024@gmail.com>
2024-11-18 01:48:20 -0800

    routing navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './main/header.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
-    {path:'header',component:HeaderComponent},
+    {path:'main',component:HeaderComponent},
     {path:'contact',component:ContactComponent},
 ];
