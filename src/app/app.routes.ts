9950e94465eda477ced30827746254a2c2b7a5a0
amnakamran1 <Amnakamran2024@gmail.com>
2024-11-19 22:20:15 -0800

    databinding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './main/header.component';
+import { DatabindingComponent } from './databinding/databinding.component';
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'main',component:HeaderComponent},
     {path:'contact',component:ContactComponent},
+    {path:'databinding',component:DatabindingComponent}
 ];
---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-databinding',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule],
+  templateUrl: './databinding.component.html',
+  styleUrl: './databinding.component.css'
+})
+export class DatabindingComponent {
+student={
+  name: "Jinha",
+  age: 25,
+  address: "Karachi",}
+  
+  message: string= "Hello world!";
+  Changemessage(){
+    this.message = "Goodbye world!";
+  }
+  num1: number=100;
+  num2: number=200;
+
+  onchange(value: string) {
+    this.message = value;
+  }
+updateMessage() {
+    this.message = 'Updated message !';
+  }
+
+}
+
